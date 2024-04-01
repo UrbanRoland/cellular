920a0ce30ebeca5d96efaad8215706d656f21bc8
Urban Roland <urolir@gmail.com>
2024-04-01 12:26:56 +0200

    init project

---
+++ src/main/java/com/bilingjob/BilingJobApplication.java
@@ -0,0 +1,13 @@
+package com.bilingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilingjob/BilingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
