771553d3dafdde5cc6cadd90ed5b407a95020e79
Urban Roland <urolir@gmail.com>
2024-04-06 13:36:10 +0200

    added fault-tolerant feature to step2

--- src/main/java/com/bilingjob/BillingJobConfiguration/BillingJobConfiguration.java
+++ src/main/java/com/bilingjob/BillingJobConfiguration/BillingJobConfiguration.java
@@ -5,6 +5,7 @@ import com.bilingjob.Records.BillingData;
 import com.bilingjob.Records.ReportingData;
 import com.bilingjob.Tasklets.FilePreparationTasklet;
 //import com.bilingjob.jobs.BillingJob;
+import com.bilingjob.jobs.BillingDataSkipListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -20,6 +21,7 @@ import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilde
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -52,11 +54,17 @@ public class BillingJobConfiguration {
     @Bean
     public Step step2(
         JobRepository jobRepository, JdbcTransactionManager transactionManager,
-        ItemReader<BillingData> billingDataFileReader, ItemWriter<BillingData> billingDataTableWriter) {
+        ItemReader<BillingData> billingDataFileReader,
+        ItemWriter<BillingData> billingDataTableWriter,
+        BillingDataSkipListener skipListener) {
         return new StepBuilder("fileIngestion", jobRepository)
             .<BillingData, BillingData>chunk(100, transactionManager)
             .reader(billingDataFileReader)
             .writer(billingDataTableWriter)
+            .faultTolerant()
+            .skip(FlatFileParseException.class)
+            .skipLimit(10)
+            .listener(skipListener)
             .build();
     }
     
@@ -125,6 +133,12 @@ public class BillingJobConfiguration {
             .delimited()
             .names("billingData.dataYear", "billingData.dataMonth", "billingData.accountId", "billingData.phoneNumber", "billingData.dataUsage", "billingData.callDuration", "billingData.smsCount", "billingTotal")
             .build();
+    }
+    
+    @Bean
+    @StepScope
+    public BillingDataSkipListener skipListener(@Value("#{jobParameters['skip.file']}") String skippedFile) {
+        return new BillingDataSkipListener(skippedFile);
     }
 
 
